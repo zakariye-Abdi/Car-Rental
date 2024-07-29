641fa28b4a4df4c742f08efcb6201b487b2f1f35
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 19:56:10 +0300

    commit

--- src/Pages/LogInSection.jsx
+++ src/Pages/LogInSection.jsx
@@ -1,14 +1,19 @@
-import React from 'react'
-import LogInHero from '../components/Log In Section/LogInHero'
-import LogInFrom from '../components/Log In Section/LogInFrom'
+import React from 'react';
+import LogInHero from '../components/Log In Section/LogInHero';
+import LogInFrom from '../components/Log In Section/LogInFrom';
 
 function LogInSection() {
   return (
     <>
-     <LogInHero /> 
-     <LogInFrom />
+      {/* Login Hero Section */}
+      {/* This component typically includes a hero image or banner with login-related messaging or branding */}
+      <LogInHero />
+
+      {/* Login Form Section */}
+      {/* This component includes the actual login form where users can enter their credentials */}
+      <LogInFrom />
     </>
-  )
+  );
 }
 
-export default LogInSection
+export default LogInSection;
