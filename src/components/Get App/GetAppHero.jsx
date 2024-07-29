34d13612ce5b16774ced02931fdcdbdc17f8caf7
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 16:41:20 +0300

    commit

--- src/components/Get App/GetAppHero.jsx
+++ src/components/Get App/GetAppHero.jsx
@@ -1,29 +1,35 @@
 import React from "react";
-import HeroBg from "../Get App/VehhicleListing Hero Bg.jpeg"
+import HeroBg from "../Get App/VehhicleListing Hero Bg.jpeg"; // Background image for the hero section
 
 function GetAppHero() {
   return (
     <>
+      {/* Main container for the hero section */}
       <div className="vehicle-hero-container w-full h-[50vh] bg-red-500 relative">
+        
+        {/* Background image for the hero section */}
         <img
           src={HeroBg}
-          className="w-full h-full  object-cover  object-[center_center]"
-          alt=""
+          className="w-full h-full object-cover object-[center_center]"
+          alt="Hero Background"
         />
 
+        {/* Overlay and content container */}
         <div className="bg-[#000000c9] absolute top-0 w-full h-full flex justify-center items-center flex-col">
+          
+          {/* Hero section heading */}
           <h1 className="uppercase text-white text-4xl font-bold">
             Get Our App
           </h1>
+
+          {/* Hero section subheading */}
           <p className="text-white tracking-widest mt-4 text-sm uppercase">
             Browse Our Inventory
           </p>
-
-         
         </div>
-      </div> 
+      </div>
     </>
-  )
+  );
 }
 
-export default GetAppHero
+export default GetAppHero;
