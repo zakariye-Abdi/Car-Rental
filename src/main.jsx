84ac9f5a2ca9f5332c9cf30bd8296699ae3fd8bf
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 19:17:22 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
 
+// Create the root element for React
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    {/* Render the App component inside the root element */}
     <App />
   </React.StrictMode>,
-)
+);
