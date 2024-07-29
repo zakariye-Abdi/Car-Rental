6416804f2585dcba93ac8c4d0f4d55e9dbd65373
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 18:52:03 +0300

    commit

--- src/components/Newest car fleet/NewestCarCard.jsx
+++ src/components/Newest car fleet/NewestCarCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 // Import Swiper React components
 import { Swiper, SwiperSlide } from "swiper/react";
 
@@ -8,46 +8,47 @@ import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
 
 import "../Newest car fleet/NewestCarCard.css";
-import SliderImg1 from '../Newest car fleet/Slider Img 1.jpeg'
-import SliderImg2 from '../Newest car fleet/Slider Img 2.jpeg'
-import SliderImg3 from '../Newest car fleet/Slider Img 3.jpeg'
-import SliderImg4 from '../Newest car fleet/Slider Img 4.jpeg'
+import SliderImg1 from '../Newest car fleet/Slider Img 1.jpeg';
+import SliderImg2 from '../Newest car fleet/Slider Img 2.jpeg';
+import SliderImg3 from '../Newest car fleet/Slider Img 3.jpeg';
+import SliderImg4 from '../Newest car fleet/Slider Img 4.jpeg';
 
-// import required modules
+// Import required modules
 import { EffectCoverflow, Pagination } from "swiper/modules";
 
 export default function NewestCarCard() {
   return (
     <>
+      {/* Swiper component for carousel effect */}
       <Swiper
-        effect={"coverflow"}
-        grabCursor={true}
-        centeredSlides={true}
-        slidesPerView={"auto"}
+        effect={"coverflow"} // Set the effect to coverflow
+        grabCursor={true} // Show the grab cursor when hovering over slides
+        centeredSlides={true} // Center the active slide
+        slidesPerView={"auto"} // Automatically adjust the number of slides per view
         coverflowEffect={{
-          rotate: 50,
-          stretch: 0,
-          depth: 100,
-          modifier: 1,
-          slideShadows: true,
+          rotate: 50, // Rotate effect
+          stretch: 0, // Stretch effect
+          depth: 100, // Depth effect
+          modifier: 1, // Modifier for effect
+          slideShadows: true, // Enable slide shadows
         }}
-        pagination={true}
-        modules={[EffectCoverflow, Pagination]}
-        className="mySwiper"
+        pagination={true} // Show pagination bullets
+        modules={[EffectCoverflow, Pagination]} // Include required modules
+        className="mySwiper" // Class for custom styling
       >
+        {/* Each SwiperSlide represents an individual slide */}
         <SwiperSlide>
-          <img src={SliderImg1} />
+          <img src={SliderImg1} alt="Slider Image 1" /> {/* Slide 1 image */}
         </SwiperSlide>
         <SwiperSlide>
-          <img src={SliderImg2} />
+          <img src={SliderImg2} alt="Slider Image 2" /> {/* Slide 2 image */}
         </SwiperSlide>
         <SwiperSlide>
-          <img src={SliderImg3} />
+          <img src={SliderImg3} alt="Slider Image 3" /> {/* Slide 3 image */}
         </SwiperSlide>
         <SwiperSlide>
-          <img src={SliderImg4} />
+          <img src={SliderImg4} alt="Slider Image 4" /> {/* Slide 4 image */}
         </SwiperSlide>
-       
       </Swiper>
     </>
   );
