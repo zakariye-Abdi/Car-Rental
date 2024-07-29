38e22c44c9290a956c780aed13d2da7cb625bc58
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 19:48:52 +0300

    commit

--- src/Pages/DealerLocation.jsx
+++ src/Pages/DealerLocation.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from 'react';
 
 function DealerLocation() {
   return (
     <div>
+      {/* Container for the Dealer Location page */}
       DealerLocation
     </div>
-  )
+  );
 }
 
-export default DealerLocation
+export default DealerLocation;
