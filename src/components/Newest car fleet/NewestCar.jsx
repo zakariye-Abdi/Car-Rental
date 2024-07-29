b3be76efc34ad18f38a7268a68fd2cdc163ce280
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 18:48:05 +0300

    commit

--- src/components/Newest car fleet/NewestCar.jsx
+++ src/components/Newest car fleet/NewestCar.jsx
@@ -1,19 +1,25 @@
-import React from 'react'
-import NewestCarCard from './NewestCarCard'
+import React from 'react';
+import NewestCarCard from './NewestCarCard';
 
 function NewestCar() {
   return (
     <>
-      <div className="newest-car-container w-full min-h-[80vh] flex flex-col justify-center items-center overflow-hidden ">
-     <div className="headings text-center ">
-     <h2 className='mb-2 text-sm tracking-wider uppercase'>Cutting-Edge Automotive Marvels</h2>
-        <h1 className='text-4xl font-bold max-md:mb-10'>NEWEST CARS FLEET</h1>
-     </div>
+      {/* Container for the newest car section */}
+      <div className="newest-car-container w-full min-h-[80vh] flex flex-col justify-center items-center overflow-hidden">
+        
+        {/* Headings section */}
+        <div className="headings text-center">
+          {/* Subheading */}
+          <h2 className='mb-2 text-sm tracking-wider uppercase'>Cutting-Edge Automotive Marvels</h2>
+          {/* Main heading */}
+          <h1 className='text-4xl font-bold max-md:mb-10'>NEWEST CARS FLEET</h1>
+        </div>
 
-     <NewestCarCard />
+        {/* Component to display the newest car details */}
+        <NewestCarCard />
       </div>
     </>
-  )
+  );
 }
 
-export default NewestCar
+export default NewestCar;
