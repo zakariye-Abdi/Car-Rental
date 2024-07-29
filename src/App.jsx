ff39320998e1353f16d86c4b177c7a2dbbd19783
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 19:15:55 +0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,55 @@
-import React from 'react'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import Layout from './Pages/Layout'
-import Home from './Pages/Home'
-import VechicleListing from './Pages/VechicleListing'
-import DealerLocation from './Pages/DealerLocation'
-import GetApp from './Pages/GetApp'
-import OurNews from './Pages/OurNews'
-import Contact from './Pages/Contact'
-import "./index.css"
-import LogInSection from './Pages/LogInSection'
-import SignUp from './Pages/SignUp'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Pages/Layout';
+import Home from './Pages/Home';
+import VechicleListing from './Pages/VechicleListing';
+import DealerLocation from './Pages/DealerLocation';
+import GetApp from './Pages/GetApp';
+import OurNews from './Pages/OurNews';
+import Contact from './Pages/Contact';
+import "./index.css";
+import LogInSection from './Pages/LogInSection';
+import SignUp from './Pages/SignUp';
 
 function App() {
   return (
     <>
-     <BrowserRouter>
-     <Routes >
-     <Route  path='/' element={<Layout/>}> 
-     <Route path='/' element={<Home/>}/>
-     <Route path='/vechileListing' element={<VechicleListing/>} />
-     <Route path='/dealerLocation' element={<DealerLocation />} />
-     <Route path='/getApp' element={<GetApp />} />
-     <Route path='/ourNews' element={<OurNews />} />
-     <Route path='/contact' element={<Contact />} />
-     <Route path='/loginUser' element={<LogInSection />} />
-     <Route path='/signup' element={<SignUp />} />
-     </Route> 
-     </Routes>
-     </BrowserRouter> 
-   
+      {/* BrowserRouter provides routing functionality for the application */}
+      <BrowserRouter>
+        {/* Routes define the available routes in the application */}
+        <Routes>
+          {/* Layout component wraps all the pages and provides common structure */}
+          <Route path='/' element={<Layout />}>
+            
+            {/* Route for the home page */}
+            <Route path='/' element={<Home />} />
+            
+            {/* Route for vehicle listing page */}
+            <Route path='/vechileListing' element={<VechicleListing />} />
+            
+            {/* Route for dealer location page */}
+            <Route path='/dealerLocation' element={<DealerLocation />} />
+            
+            {/* Route for the get app page */}
+            <Route path='/getApp' element={<GetApp />} />
+            
+            {/* Route for news page */}
+            <Route path='/ourNews' element={<OurNews />} />
+            
+            {/* Route for contact page */}
+            <Route path='/contact' element={<Contact />} />
+            
+            {/* Route for login page */}
+            <Route path='/loginUser' element={<LogInSection />} />
+            
+            {/* Route for sign-up page */}
+            <Route path='/signup' element={<SignUp />} />
+            
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
-
+export default App;
