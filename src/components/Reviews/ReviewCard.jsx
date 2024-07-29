9c64314dd9189137fad1e3cb920875aff56c7dc2
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 19:02:45 +0300

    commit

--- src/components/Reviews/ReviewCard.jsx
+++ src/components/Reviews/ReviewCard.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState } from 'react';
+import React from 'react';
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
@@ -8,26 +8,32 @@ import 'swiper/css/pagination';
 
 import '../Reviews/ReviewCard.css';
 
-// import required modules
+// Import required modules from Swiper
 import { Pagination } from 'swiper/modules';
 import ReviewsCardDetails from './ReviewsCardDetails';
 
 export default function ReviewCard() {
   return (
-    <div className='flex flex-col justify-center items-center  w-[100rem] pb-10 max-lg:w-[80rem] max-md:w-[70rem] max-[660px]:w-[60rem] h-full'>
+    <div className='flex flex-col justify-center items-center w-[100rem] pb-10 max-lg:w-[80rem] max-md:w-[70rem] max-[660px]:w-[60rem] h-full'>
+      {/* Swiper component for the review carousel */}
       <Swiper 
-        spaceBetween={30}
+        spaceBetween={30} // Space between slides
         pagination={{
-          clickable: true,
+          clickable: true, // Enable clickable pagination bullets
         }}
-        modules={[Pagination]}
+        modules={[Pagination]} // Include the Pagination module
         className="mySwiper min-h-[50vh] max-md:h-[30vh] overflow-hidden max-[550px]:w-[50rem] h-full"
       >
-        <SwiperSlide> <ReviewsCardDetails /></SwiperSlide>
-        <SwiperSlide> <ReviewsCardDetails /></SwiperSlide>
-        <SwiperSlide> <ReviewsCardDetails /></SwiperSlide>
-       
-       
+        {/* Each SwiperSlide represents an individual slide */}
+        <SwiperSlide>
+          <ReviewsCardDetails /> {/* Content of the slide */}
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewsCardDetails /> {/* Content of the slide */}
+        </SwiperSlide>
+        <SwiperSlide>
+          <ReviewsCardDetails /> {/* Content of the slide */}
+        </SwiperSlide>
       </Swiper>
     </div>
   );
