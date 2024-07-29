d09dde98971bef1f69c21750ca2e2ad7d6b51a3b
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 20:07:23 +0300

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 
 // Create the root element for React
+// The root element is where the React application will be mounted
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* Render the App component inside the root element */}
+    {/* React.StrictMode is a wrapper that helps detect potential problems in the application */}
     <App />
   </React.StrictMode>,
 );
