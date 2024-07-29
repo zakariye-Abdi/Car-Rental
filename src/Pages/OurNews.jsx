6318c3e25b080cc76b9013d8ccc88022d7accaaa
zakariye-Abdi <Zakariye639@gmail.com>
2024-07-29 19:57:17 +0300

    commit

--- src/Pages/OurNews.jsx
+++ src/Pages/OurNews.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 function OurNews() {
   return (
     <div>
+      {/* Main Container for OurNews Section */}
+      {/* This div serves as a wrapper for the "Our News" section content */}
       OurNews
     </div>
-  )
+  );
 }
 
-export default OurNews
+export default OurNews;
